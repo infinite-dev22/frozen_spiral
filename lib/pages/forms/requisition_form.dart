42f17ec3cfb76727389d25b71ae9ee4bfcde5743
infinite-dev22 <mwigojm@gmail.com>
2023-10-23 11:08:23 +0300

    Change currency and approvers async bottom sheets to dropdowns. Add file financial status. Add thousands formatter on requisition amount. Set currency default value to Uganda Shillings

--- lib/pages/forms/requisition_form.dart
+++ lib/pages/forms/requisition_form.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:smart_case/models/smart_currency.dart';
 import 'package:smart_case/widgets/custom_accordion.dart';
+import 'package:smart_case/widgets/custom_dropdowns.dart';
 import 'package:smart_case/widgets/custom_textbox.dart';
+import 'package:toast/toast.dart';
 
 import '../../models/smart_file.dart';
 import '../../models/smart_requisition.dart';
@@ -10,28 +12,33 @@ import '../../services/apis/smartcase_api.dart';
 import '../../theme/color.dart';
 import '../../util/smart_case_init.dart';
 import '../../widgets/custom_searchable_async_bottom_sheet_contents.dart';
+import '../../widgets/form_title.dart';
 
 class RequisitionForm extends StatefulWidget {
   const RequisitionForm({
     super.key,
-    this.onSave,
+    this.onSave, required this.currencies,
   });
 
   final Function()? onSave;
+  final List<SmartCurrency> currencies;
 
   @override
   State<RequisitionForm> createState() => _RequisitionFormState();
 }
 
 class _RequisitionFormState extends State<RequisitionForm> {
+  final globalKey = GlobalKey();
+  final ToastContext toast = ToastContext();
+
   final TextEditingController dateController = TextEditingController();
   final TextEditingController amountController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
 
-  List<SmartCurrency> currencies = List.empty(growable: true);
   List<SmartFile> files = List.empty(growable: true);
   List<SmartUser> approvers = List.empty(growable: true);
   List<SmartRequisitionCategory> categories = List.empty(growable: true);
+  int financialStatus = 0;
 
   SmartFile? file;
   SmartCurrency? currency;
@@ -40,100 +47,106 @@ class _RequisitionFormState extends State<RequisitionForm> {
 
   @override
   Widget build(BuildContext context) {
+    toast.init(context);
+
     return _buildBody();
   }
 
   _buildBody() {
-    return Expanded(
-      child: NotificationListener(
-        onNotification: (notification) {
-          if (notification is ScrollEndNotification) {
-            FocusManager.instance.primaryFocus?.unfocus();
-            return true;
-          }
-          return false;
-        },
-        child: ListView(
-          padding: const EdgeInsets.all(16),
-          children: [
-            DateAccordion(
-              dateController: dateController,
-            ),
-            Container(
-              height: 50,
-              width: double.infinity,
-              padding: const EdgeInsets.all(5),
-              margin: const EdgeInsets.only(bottom: 10),
-              decoration: BoxDecoration(
-                color: AppColors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextButton(
-                onPressed: _showSearchCurrencyBottomSheet,
-                child: Text(
-                  currency?.getName() ?? 'Select currency',
-                ),
-              ),
-            ),
-            Container(
-              height: 50,
-              width: double.infinity,
-              padding: const EdgeInsets.all(5),
-              margin: const EdgeInsets.only(bottom: 10),
-              decoration: BoxDecoration(
-                color: AppColors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextButton(
-                onPressed: _showSearchFileBottomSheet,
-                child: Text(
-                  file?.getName() ?? 'Select file',
-                ),
-              ),
-            ),
-            if (file != null)
-              const SmartText(
-                  value: '\$100,000 (Dummy data)',
-                  icon: Icons.arrow_upward_rounded,
-                  color: AppColors.green),
-            Container(
-              height: 50,
-              width: double.infinity,
-              padding: const EdgeInsets.all(5),
-              margin: const EdgeInsets.only(bottom: 10),
-              decoration: BoxDecoration(
-                color: AppColors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextButton(
-                onPressed: _showSearchApproverBottomSheet,
-                child: Text(
-                  approver?.getName() ?? 'Select approver',
-                ),
-              ),
-            ),
-            Container(
-              height: 50,
-              width: double.infinity,
-              padding: const EdgeInsets.all(5),
-              margin: const EdgeInsets.only(bottom: 10),
-              decoration: BoxDecoration(
-                color: AppColors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextButton(
-                onPressed: _showSearchCategoryBottomSheet,
-                child: Text(
-                  category?.getName() ?? 'Select requisition category',
-                ),
-              ),
-            ),
-            SmartCaseTextField(hint: 'Amount', controller: amountController),
-            CustomTextArea(
-                hint: 'Description', controller: descriptionController),
-          ],
+    return Column(
+      children: [
+        FormTitle(
+          name: 'New Requisition',
+          onSave: _submitFormData,
         ),
-      ),
+        Expanded(
+          child: NotificationListener(
+            onNotification: (notification) {
+              if (notification is ScrollEndNotification) {
+                FocusManager.instance.primaryFocus?.unfocus();
+                return true;
+              }
+              return false;
+            },
+            child: ListView(
+              padding: const EdgeInsets.all(16),
+              children: [
+                DateAccordion(
+                  dateController: dateController,
+                ),
+                CustomDropdown<SmartCurrency>(
+                    hintText: 'currency',
+                    menuItems: widget.currencies,
+                    defaultValue: widget.currencies
+                        .firstWhere((currency) => currency.code == 'UGX'),
+                    onChanged: _onTapSearchedCurrency),
+                Container(
+                  height: 50,
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(5),
+                  margin: const EdgeInsets.only(bottom: 10),
+                  decoration: BoxDecoration(
+                    color: AppColors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextButton(
+                    onPressed: _showSearchFileBottomSheet,
+                    child: Text(
+                      file?.getName() ?? 'Select file',
+                    ),
+                  ),
+                ),
+                if (file != null)
+                  SmartText(
+                      value: financialStatus.toString(),
+                      icon: (financialStatus > 0)
+                          ? Icons.arrow_upward_rounded
+                          : (financialStatus < 0)
+                              ? Icons.arrow_downward_rounded
+                              : Icons.circle_outlined,
+                      color: (financialStatus > 0)
+                          ? AppColors.green
+                          : (financialStatus < 0)
+                              ? AppColors.red
+                              : AppColors.blue),
+                CustomDropdown<SmartUser>(
+                  hintText: 'approver',
+                  menuItems: approvers,
+                  onChanged: _onTapSearchedApprover,
+                ),
+                Container(
+                  height: 50,
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(5),
+                  margin: const EdgeInsets.only(bottom: 10),
+                  decoration: BoxDecoration(
+                    color: AppColors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextButton(
+                    onPressed: _showSearchCategoryBottomSheet,
+                    child: Text(
+                      category?.getName() ?? 'Select requisition category',
+                    ),
+                  ),
+                ),
+                SmartCaseNumberField(
+                    hint: 'Amount', controller: amountController),
+                CustomTextArea(
+                  key: globalKey,
+                  hint: 'Description',
+                  controller: descriptionController,
+                  onTap: () {
+                    Scrollable.ensureVisible(globalKey.currentContext!);
+                  },
+                ),
+                const SizedBox(
+                    height: 300 /* MediaQuery.of(context).viewInsets.bottom */),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -168,92 +181,6 @@ class _RequisitionFormState extends State<RequisitionForm> {
                             .contains(value.toLowerCase())));
                       } else {
                         _loadFiles();
-                        isLoading = true;
-                      }
-                    }
-                  });
-                },
-                isLoading: isLoading,
-              );
-            },
-          );
-        });
-  }
-
-  _showSearchCurrencyBottomSheet() {
-    List<SmartCurrency> searchedList = List.empty(growable: true);
-    bool isLoading = false;
-    return showModalBottomSheet(
-        isScrollControlled: true,
-        constraints: BoxConstraints.expand(
-            height: MediaQuery.of(context).size.height * .8),
-        context: context,
-        builder: (BuildContext context) {
-          return StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-              return AsyncSearchableBottomSheetContents(
-                hint: "Search currency",
-                list: searchedList,
-                onTap: (value) {
-                  _onTapSearchedCurrency(value!);
-                  Navigator.pop(context);
-                },
-                onSearch: (value) {
-                  setState(() {
-                    searchedList.clear();
-                    if (value.length > 2) {
-                      if (currencies.isNotEmpty) {
-                        isLoading = false;
-                        searchedList.addAll(currencies.where((smartCurrency) =>
-                            smartCurrency
-                                .getName()
-                                .toLowerCase()
-                                .contains(value.toLowerCase())));
-                      } else {
-                        _loadCurrencies();
-                        isLoading = true;
-                      }
-                    }
-                  });
-                },
-                isLoading: isLoading,
-              );
-            },
-          );
-        });
-  }
-
-  _showSearchApproverBottomSheet() {
-    List<SmartUser> searchedList = List.empty(growable: true);
-    bool isLoading = false;
-    return showModalBottomSheet(
-        isScrollControlled: true,
-        constraints: BoxConstraints.expand(
-            height: MediaQuery.of(context).size.height * .8),
-        context: context,
-        builder: (BuildContext context) {
-          return StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-              return AsyncSearchableBottomSheetContents(
-                hint: "Search approvers",
-                list: searchedList,
-                onTap: (value) {
-                  _onTapSearchedApprover(value!);
-                  Navigator.pop(context);
-                },
-                onSearch: (value) {
-                  setState(() {
-                    searchedList.clear();
-                    if (value.length > 2) {
-                      if (approvers.isNotEmpty) {
-                        isLoading = false;
-                        searchedList.addAll(approvers.where((smartApprover) =>
-                            smartApprover
-                                .getName()
-                                .toLowerCase()
-                                .contains(value.toLowerCase())));
-                      } else {
-                        _loadApprovers();
                         isLoading = true;
                       }
                     }
@@ -314,17 +241,6 @@ class _RequisitionFormState extends State<RequisitionForm> {
     setState(() {});
   }
 
-  _loadCurrencies() async {
-    Map currencyMap = await SmartCaseApi.smartFetch(
-        'api/admin/currencytypes', currentUser.token);
-    List? currencyList = currencyMap["currencytypes"];
-    setState(() {
-      currencies =
-          currencyList!.map((doc) => SmartCurrency.fromJson(doc)).toList();
-      currencyList = null;
-    });
-  }
-
   _loadApprovers() async {
     Map usersMap =
         await SmartCaseApi.smartFetch('api/hr/employees', currentUser.token);
@@ -349,20 +265,33 @@ class _RequisitionFormState extends State<RequisitionForm> {
     });
   }
 
+  _loadFileFinancialStatus() async {
+    Map financialStatusMap = await SmartCaseApi.smartFetch(
+        'api/cases/${file!.getId()}/contactsandfinancialstatus/',
+        currentUser.token);
+
+    int? financialStatus = financialStatusMap['caseFinancialStatus'];
+    setState(() {
+      this.financialStatus = financialStatus!;
+      financialStatus = null;
+    });
+  }
+
   _onTapSearchedFile(SmartFile value) {
     setState(() {
       file = value;
       _loadApprovers();
+      _loadFileFinancialStatus();
     });
   }
 
-  _onTapSearchedCurrency(SmartCurrency value) {
+  _onTapSearchedCurrency(SmartCurrency? value) {
     setState(() {
       currency = value;
     });
   }
 
-  _onTapSearchedApprover(SmartUser value) {
+  _onTapSearchedApprover(SmartUser? value) {
     setState(() {
       approver = value;
     });
@@ -377,10 +306,37 @@ class _RequisitionFormState extends State<RequisitionForm> {
   @override
   void initState() {
     _loadFiles();
-    _loadCurrencies();
     _loadApprovers();
     _loadCategories();
 
     super.initState();
+  }
+
+  _submitFormData() {
+    SmartRequisition smartRequisition = SmartRequisition(
+        date: dateController.text.trim(),
+        amount: amountController.text.trim(),
+        payoutAmount: financialStatus.toString(),
+        description: descriptionController.text.trim(),
+        employeeId: currentUser.getId(),
+        supervisorId: approver!.getId(),
+        requisitionStatusId: 5,
+        requisitionCategoryId: category!.getId(),
+        currencyId: currency!.getId());
+
+    SmartCaseApi.smartPost(
+      'api/accounts/cases/${file!.getId()}/requisitions',
+      currentUser.token,
+      smartRequisition.toJson(),
+      onError: () {
+        Toast.show("An error occurred",
+            duration: Toast.lengthLong, gravity: Toast.bottom);
+      },
+      onSuccess: () {
+        Toast.show("Requisition added successfully",
+            duration: Toast.lengthLong, gravity: Toast.bottom);
+        Navigator.pop(context);
+      },
+    );
   }
 }
