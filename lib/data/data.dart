9a86c562d466cfe6b6b8efaf2b25101f9540a76f
infinite-dev22 <mwigojm@gmail>
2023-11-28 16:10:27 +0300

    🤏 Minor code cleanup 🧹 👉 Affected files 📄: - data.dart

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-String userEmail = 'user@testmail.com';
-String userPassword = 'user12345';
-
 List modules = [
   {'icon': Icons.local_activity_outlined, 'name': 'Activities'},
   {'icon': Icons.list_rounded, 'name': 'Requisitions'},
@@ -11,21 +8,3 @@ List modules = [
   {'icon': Icons.handshake_outlined, 'name': 'Engagements'},
   {'icon': Icons.bar_chart_rounded, 'name': 'Reports'},
 ];
-
-List<String> activityList = [
-  "Test name 0",
-  "Test name 1",
-  "Test name 2",
-  "Test name 3",
-  "Test name 4",
-  "Test name 5",
-  "Test name 6",
-  "Test name 7",
-  "Test name 8",
-  "Test name 9",
-  "Test name 10",
-  "Test name 11",
-];
-
-List notifications = [];
-List locations = [];
