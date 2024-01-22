e917586814fb5b22a6ef8bd00622e237bb4cb560
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-01-22 09:38:23 +0300

    RELEASE: => Release app version 2024.1.1+19 for android.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import firebase_core
 import firebase_messaging
 import flutter_image_compress_macos
 import light_compressor
+import package_info_plus
 import path_provider_foundation
 import record_macos
 import rive_common
@@ -20,6 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   LightCompressorPlugin.register(with: registry.registrar(forPlugin: "LightCompressorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacosPlugin.register(with: registry.registrar(forPlugin: "RecordMacosPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
